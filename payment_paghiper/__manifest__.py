326fc2f435f2f602b904e34f8c9eb321061b79ec
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-07-17 19:10:53 -0300

    Removing wrong files

--- payment_paghiper/__manifest__.py
+++ payment_paghiper/__manifest__.py
@@ -4,6 +4,7 @@
     'description': """PagHiper payment gateway for Odoo.""",
     'author': "Danimar Ribeiro",
     'category': 'Accounting',
+    'license': 'Other OSI approved licence',
     'version': '13.0.1.0.0',
     'depends': ['l10n_br_automated_payment', 'payment', 'sale'],
     'data': [
