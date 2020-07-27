3ca29bb3fe79788be76cc19b5ea449fc670acbbf
Felipe Paloschi <paloschi.eca@gmail.com>
2020-07-27 17:21:12 -0300

    delivery_correios: Rename stock_pack_operation to stock_move Format __manifest__ file using black formatter

--- delivery_correios/__manifest__.py
+++ delivery_correios/__manifest__.py
@@ -2,32 +2,30 @@
 # Part of Trustcode. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': "Delivery Correios",
-    'summary': """Integração com os Correios""",
-    'description': """Módulo para gerar etiquetas de rastreamento de \
+    "name": "Delivery Correios",
+    "summary": """Integração com os Correios""",
+    "description": """Módulo para gerar etiquetas de rastreamento de \
 encomendas""",
-    'version': '13.0.1.0.0',
-    'category': 'MRP',
-    'author': 'Code 137',
-    'license': 'Other OSI approved licence',
-    'website': 'http://www.code137.com.br',
-    'contributors': [
-        'Danimar Ribeiro <danimaribeiro@gmail.com>',
-        'Felipe Paloschi <paloschi.eca@gmail.com>',
+    "version": "13.0.1.0.0",
+    "category": "MRP",
+    "author": "Code 137",
+    "license": "Other OSI approved licence",
+    "website": "http://www.code137.com.br",
+    "contributors": [
+        "Danimar Ribeiro <danimaribeiro@gmail.com>",
+        "Felipe Paloschi <paloschi.eca@gmail.com>",
     ],
-    'depends': [
-        'stock', 'delivery'
+    "depends": ["stock", "delivery"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/correios.xml",
+        "views/delivery_carrier.xml",
+        "views/sale_order.xml",
+        "views/product_template.xml",
+        "views/stock_picking.xml",
+        "reports/shipping_label.xml",
+        "reports/plp_report.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/correios.xml',
-        'views/delivery_carrier.xml',
-        'views/sale_order.xml',
-        'views/product_template.xml',
-        'views/stock_picking.xml',
-        'reports/shipping_label.xml',
-        'reports/plp_report.xml',
-    ],
-    'application': True,
-    'installable': True,
+    "application": True,
+    "installable": True,
 }
