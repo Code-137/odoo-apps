962516c8f3a278ac8761b90447f428886faf8232
Felipe Paloschi <paloschi.eca@gmail.com>
2020-08-04 13:59:33 -0300

    Fix method that gets the normal shipping rate.
    Format delivery.py using black.

--- delivery_correios/models/delivery.py
+++ delivery_correios/models/delivery.py
@@ -3,10 +3,8 @@
 
 import re
 import logging
-import requests
-from lxml import objectify
 from datetime import datetime
-from odoo import api, fields, models
+from odoo import fields, models
 from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
@@ -14,20 +12,20 @@ _logger = logging.getLogger(__name__)
 try:
     from pysigep.client import SOAPClient
 except ImportError:
-    _logger.warning('Cannot import pysigep')
+    _logger.warning("Cannot import pysigep")
 
 
 def check_for_correio_error(method):
-    if 'mensagem_erro' in method:
-        raise UserError(method['mensagem_erro'])
-    elif 'erro' in method:
-        raise UserError(method['erro'])
-    elif hasattr(method, 'cServico') and int(method.cServico.Erro) != 0:
+    if "mensagem_erro" in method:
+        raise UserError(method["mensagem_erro"])
+    elif "erro" in method:
+        raise UserError(method["erro"])
+    elif hasattr(method, "cServico") and int(method.cServico.Erro) != 0:
         raise UserError(method.cServico.MsgErro)
 
 
 class DeliveryCarrier(models.Model):
-    _inherit = 'delivery.carrier'
+    _inherit = "delivery.carrier"
 
     has_contract = fields.Boolean(string="Tem Contrato?")
     correio_login = fields.Char(string=u"Login Correios", size=30)
@@ -35,215 +33,281 @@ class DeliveryCarrier(models.Model):
     cod_administrativo = fields.Char(string=u"Código Administrativo", size=20)
     num_contrato = fields.Char(string=u"Número de Contrato", size=20)
     cartao_postagem = fields.Char(
-        string=u"Número do cartão de Postagem", size=20)
+        string=u"Número do cartão de Postagem", size=20
+    )
 
-    delivery_type = fields.Selection(selection_add=[('correios', u'Correios')])
+    delivery_type = fields.Selection(
+        selection_add=[("correios", u"Correios")]
+    )
     # Type without contract
-    service_type = fields.Selection([
-        ('04014', 'Sedex'),
-        ('04510', 'PAC'),
-        ('04782', 'Sedex 12'),
-        ('04790', 'Sedex 10'),
-        ('04804', 'Sedex Hoje'),
-    ], string="Tipo de Entrega")
+    service_type = fields.Selection(
+        [
+            ("04014", "Sedex"),
+            ("04510", "PAC"),
+            ("04782", "Sedex 12"),
+            ("04790", "Sedex 10"),
+            ("04804", "Sedex Hoje"),
+        ],
+        string="Tipo de Entrega",
+        help="Tipo de entrega utilizado quando a empresa não possui contrato \
+com o Correio",
+    )
     # Type for those who have contract
     service_id = fields.Many2one(
-        'delivery.correios.service', string="Serviço")
-    mao_propria = fields.Selection([('S', 'Sim'), ('N', 'Não')],
-                                   string='Entregar em Mão Própria')
-    valor_declarado = fields.Boolean('Valor Declarado')
-    aviso_recebimento = fields.Selection([('S', 'Sim'), ('N', 'Não')],
-                                         string='Receber Aviso de Entrega')
-    ambiente = fields.Selection([('1', 'Homologação'), ('2', 'Produção')],
-                                default='1', string="Ambiente")
+        "delivery.correios.service", string="Serviço"
+    )
+    mao_propria = fields.Selection(
+        [("S", "Sim"), ("N", "Não")], string="Entregar em Mão Própria"
+    )
+    valor_declarado = fields.Boolean("Valor Declarado")
+    aviso_recebimento = fields.Selection(
+        [("S", "Sim"), ("N", "Não")], string="Receber Aviso de Entrega"
+    )
+    ambiente = fields.Selection(
+        [("1", "Homologação"), ("2", "Produção")],
+        default="1",
+        string="Ambiente",
+    )
+
+    def get_correio_soap_client(self):
+        return SOAPClient(
+            ambiente=int(self.ambiente),
+            senha=self.correio_password,
+            usuario=self.correio_login,
+        )
 
     def action_get_correio_services(self):
-        client = SOAPClient(ambiente=int(self.ambiente),
-                            senha=self.correio_password,
-                            usuario=self.correio_login)
+        client = SOAPClient(
+            ambiente=int(self.ambiente),
+            senha=self.correio_password,
+            usuario=self.correio_login,
+        )
         result = client.busca_cliente(self.num_contrato, self.cartao_postagem)
         check_for_correio_error(result)
         servicos = result["contratos"][0]["cartoesPostagem"][0]["servicos"]
         ano_assinatura = result["contratos"][0]["dataVigenciaInicio"]
         for item in servicos:
-            correio = self.env['delivery.correios.service']
-            item_correio = correio.search([('code', '=', item["codigo"])])
+            correio = self.env["delivery.correios.service"]
+            item_correio = correio.search([("code", "=", item["codigo"])])
             chancela = item["servicoSigep"]["chancela"]
 
             if item_correio:
-                item_correio.write({
-                    'name': item["descricao"],
-                    'chancela': chancela and chancela.get("chancela"),
-                    'ano_assinatura': str(ano_assinatura)[:4],
-                })
+                item_correio.write(
+                    {
+                        "name": item["descricao"],
+                        "chancela": chancela and chancela.get("chancela"),
+                        "ano_assinatura": str(ano_assinatura)[:4],
+                    }
+                )
             else:
-                correio.create({
-                    'code': item["codigo"],
-                    'identifier': item["id"],
-                    'chancela': chancela and chancela.get("chancela"),
-                    'name': item["descricao"],
-                    'delivery_id': self.id,
-                })
+                correio.create(
+                    {
+                        "code": item["codigo"],
+                        "identifier": item["id"],
+                        "chancela": chancela and chancela.get("chancela"),
+                        "name": item["descricao"],
+                        "delivery_id": self.id,
+                    }
+                )
 
     def _get_normal_shipping_rate(self, order):
-        origem = re.sub('[^0-9]', '', order.company_id.zip or '')
-        destino = re.sub('[^0-9]', '',  order.partner_shipping_id.zip or '')
+        origem = re.sub("[^0-9]", "", order.company_id.zip or "")
+        destino = re.sub("[^0-9]", "", order.partner_shipping_id.zip or "")
         total = 0.0
         messages = []
         for line in order.order_line.filtered(lambda x: not x.is_delivery):
 
-            peso = line.product_id.weight
-            comprimento = line.product_id.comprimento
-            largura = line.product_id.largura
-            altura = line.product_id.altura
-            servico = self.service_type
-            url = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?\
-sCepOrigem={0}&sCepDestino={1}&nVlPeso={2}&nCdFormato=1&\
-nVlComprimento={3}&nVlAltura={4}&nVlLargura={5}&\
-sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&\
-nCdServico={6}&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3".format(
-                    origem, destino, peso, comprimento, altura, largura, servico
-                )
-            response = requests.get(url)
-            obj = objectify.fromstring(response.text.encode('iso-8859-1'))
-            if obj.cServico.Erro == 0:
-                total += float(obj.cServico.Valor.text.replace(',', '.'))
+            params = {
+                "numero_servico": self.service_type,
+                "peso": str(line.product_id.weight),
+                "comprimento": str(line.product_id.comprimento),
+                "altura": str(line.product_id.altura),
+                "largura": str(line.product_id.largura),
+                "diametro": str(0),
+                "valor_declarado": 0,
+                "aviso_recebimento": False,
+                "mao_propria": False,
+                "formato": 1,
+                "cep_origem": origem,
+                "cep_destino": destino,
+            }
+
+            cliente = self.get_correio_soap_client()
+
+            response = cliente.calcular_preco_prazo(**params)
+
+            data = response.cServico[0]
+
+            if data.Erro == "0":
+                total += float(data.Valor.replace(",", "."))
             else:
-                messages.append('{0} - {1}'.format(line.product_id.name, obj.cServico.MsgErro))
+                messages.append(
+                    "{0} - {1}".format(line.product_id.name, data.MsgErro)
+                )
 
         if len(messages) > 0:
             return {
-                'success': True,
-                'price': 12,
-                'error_message': '\n'.join(messages),
+                "success": False,
+                "price": 12,
+                "error_message": "\n".join(messages),
             }
         else:
-          return {
-              'success': True,
-              'price': total,
-              'warning_message': 'Prazo de entrega',
-          }
+            return {
+                "success": True,
+                "price": total,
+                "warning_message": "Prazo de entrega: {} dias".format(
+                    data.PrazoEntrega
+                ),
+            }
 
     def correios_rate_shipment(self, order):
         return self._get_normal_shipping_rate(order)
 
     def correios_send_shipping(self, pickings):
-        ''' Send the package to the service provider
+        """ Send the package to the service provider
 
         :param pickings: A recordset of pickings
         :return list: A list of dictionaries (one per picking) containing of
                     the form::
                          { 'exact_price': price,
                            'tracking_number': number }
-        '''
-        client = SOAPClient(ambiente=int(self.ambiente),
-                            senha=self.correio_password,
-                            usuario=self.correio_login)
+        """
+        client = self.get_correio_soap_client()
 
-        plp = self.env['delivery.correios.postagem.plp'].search(
-            [('state', '=', 'draft')], limit=1)
+        plp = self.env["delivery.correios.postagem.plp"].search(
+            [("state", "=", "draft")], limit=1
+        )
         if not len(plp):
-            name = "%s - %s" % (self.name, datetime.now().strftime("%d-%m-%Y"))
-            plp = self.env['delivery.correios.postagem.plp'].create({
-                'name': name, 'state': 'draft',
-                'delivery_id': self.id, 'total_value': 0,
-                'company_id': self.company_id.id,
-            })
+            name = "%s - %s" % (
+                self.name,
+                datetime.now().strftime("%d-%m-%Y"),
+            )
+            plp = self.env["delivery.correios.postagem.plp"].create(
+                {
+                    "name": name,
+                    "state": "draft",
+                    "delivery_id": self.id,
+                    "total_value": 0,
+                    "company_id": self.company_id.id,
+                }
+            )
         res = []
         for picking in pickings:
-            cnpj_empresa = re.sub('[^0-9]', '', picking.company_id.l10n_br_cnpj_cpf or '')
+            cnpj_empresa = re.sub(
+                "[^0-9]", "", picking.company_id.l10n_br_cnpj_cpf or ""
+            )
 
             tags = []
             preco_soma = 0
             for pack in picking.move_line_ids_without_package:
                 usuario_preco_prazo = {
-                    'nCdEmpresa': self.cod_administrativo,
-                    'sDsSenha': self.correio_password,
-                    'nCdServico': self.service_id.code,
-                    'sCepOrigem': pack.location_id.company_id.zip,
-                    'sCepDestino': picking.partner_id.zip,
-                    'nVlPeso': pack.product_id.weight,
-                    'nVlComprimento': pack.product_id.comprimento,
-                    'nVlAltura': pack.product_id.altura,
-                    'nVlLargura': pack.product_id.largura,
-                    'nVlDiametro': pack.product_id.diametro,
-                    'nCdFormato': 1,
-                    'sCdMaoPropria': self.mao_propria,
-                    'nVlValorDeclarado': self.product_id.lst_price,
-                    'sCdAvisoRecebimento': self.aviso_recebimento,
+                    "nCdEmpresa": self.cod_administrativo,
+                    "sDsSenha": self.correio_password,
+                    "nCdServico": self.service_id.code,
+                    "sCepOrigem": pack.location_id.company_id.zip,
+                    "sCepDestino": picking.partner_id.zip,
+                    "nVlPeso": pack.product_id.weight,
+                    "nVlComprimento": pack.product_id.comprimento,
+                    "nVlAltura": pack.product_id.altura,
+                    "nVlLargura": pack.product_id.largura,
+                    "nVlDiametro": pack.product_id.diametro,
+                    "nCdFormato": 1,
+                    "sCdMaoPropria": self.mao_propria,
+                    "nVlValorDeclarado": self.product_id.lst_price,
+                    "sCdAvisoRecebimento": self.aviso_recebimento,
                 }
-                usuario_preco_prazo['ambiente'] = self.ambiente
+                usuario_preco_prazo["ambiente"] = self.ambiente
                 preco = 12  # Calcular o preco novamente
                 preco_soma += preco * pack.product_qty
 
                 etiqueta = client.solicita_etiquetas(
-                    "C", cnpj_empresa, self.service_id.identifier, 1)
+                    "C", cnpj_empresa, self.service_id.identifier, 1
+                )
                 if len(etiqueta) > 0:
                     etiqueta = etiqueta[0]
                 else:
-                    raise UserError('Nenhuma etiqueta recebida')
+                    raise UserError("Nenhuma etiqueta recebida")
                 pack.track_ref = etiqueta
                 tags.append(etiqueta)
-                self.env['delivery.correios.postagem.objeto'].create({
-                    'name': etiqueta, 'stock_pack_id': pack.id,
-                    'plp_id': plp.id, 'delivery_id': self.id,
-                })
-            tags = ';'.join(tags)
+                self.env["delivery.correios.postagem.objeto"].create(
+                    {
+                        "name": etiqueta,
+                        "stock_pack_id": pack.id,
+                        "plp_id": plp.id,
+                        "delivery_id": self.id,
+                    }
+                )
+            tags = ";".join(tags)
             pickings.carrier_tracking_ref = tags
-            res.append({'exact_price': preco_soma, 'tracking_number': tags})
+            res.append({"exact_price": preco_soma, "tracking_number": tags})
             plp.total_value = preco_soma
         return res
 
     def correios_get_tracking_link(self, pickings):
-        ''' Ask the tracking link to the service provider
+        """ Ask the tracking link to the service provider
 
         :param pickings: A recordset of pickings
         :return list: A list of string URLs, containing the tracking links
          for every picking
-        '''
+        """
         usuario = {
-            'usuario': self.correio_login,
-            'senha': self.correio_password,
+            "usuario": self.correio_login,
+            "senha": self.correio_password,
         }
-#       tracking_refs = ['PL207893158BR', 'JS535334467BR']
+        #       tracking_refs = ['PL207893158BR', 'JS535334467BR']
         for picking in pickings:
             for pack in picking.pack_operation_product_ids:
                 track_ref = [pack.track_ref]
-                usuario['objetos'] = track_ref
-                usuario['ambiente'] = self.ambiente
+                usuario["objetos"] = track_ref
+                usuario["ambiente"] = self.ambiente
                 objetos = get_eventos(**usuario)
                 check_for_correio_error(objetos)
                 objetos = objetos.objeto
                 for objeto in objetos:
-                    postagem = self.env['delivery.correios.postagem.objeto'].\
-                        search([('stock_pack_id', '=', pack.id)], limit=1)
+                    postagem = self.env[
+                        "delivery.correios.postagem.objeto"
+                    ].search([("stock_pack_id", "=", pack.id)], limit=1)
                     correio_evento = {
-                        'etiqueta': objeto.numero,
-                        'postagem_id': postagem.id
+                        "etiqueta": objeto.numero,
+                        "postagem_id": postagem.id,
                     }
-                    if hasattr(objeto, 'evento'):
+                    if hasattr(objeto, "evento"):
                         for evento in objeto.evento:
-                            correio_evento['status'] = evento.status
-                            correio_evento['data'] = datetime.strptime(
-                                str(evento.data), '%d/%m/%Y')
-                            correio_evento['local_origem'] = evento.local +\
-                                ' - ' + str(evento.codigo) + ', ' +\
-                                evento.cidade + '/' + evento.uf
-                            correio_evento['descricao'] = evento.descricao
-                            if 'destino' in evento:
-                                correio_evento['local_destino'] =\
-                                    evento.destino.local + ' - ' +\
-                                    str(evento.destino.codigo) + ', ' +\
-                                    evento.destino.cidade + '/' + evento.\
-                                    destino.uf
-                    self.env['delivery.correios.postagem.eventos'].create(
-                        correio_evento)
-        return ['/web#min=1&limit=80&view_type=list&model=delivery.\
-correios.postagem.plp&action=396']
+                            correio_evento["status"] = evento.status
+                            correio_evento["data"] = datetime.strptime(
+                                str(evento.data), "%d/%m/%Y"
+                            )
+                            correio_evento["local_origem"] = (
+                                evento.local
+                                + " - "
+                                + str(evento.codigo)
+                                + ", "
+                                + evento.cidade
+                                + "/"
+                                + evento.uf
+                            )
+                            correio_evento["descricao"] = evento.descricao
+                            if "destino" in evento:
+                                correio_evento["local_destino"] = (
+                                    evento.destino.local
+                                    + " - "
+                                    + str(evento.destino.codigo)
+                                    + ", "
+                                    + evento.destino.cidade
+                                    + "/"
+                                    + evento.destino.uf
+                                )
+                    self.env["delivery.correios.postagem.eventos"].create(
+                        correio_evento
+                    )
+        return [
+            "/web#min=1&limit=80&view_type=list&model=delivery.\
+correios.postagem.plp&action=396"
+        ]
 
     def correios_cancel_shipment(self):
-        ''' Cancel a shipment
+        """ Cancel a shipment
 
         :param pickings: A recordset of pickings
-        '''
+        """
         pass
