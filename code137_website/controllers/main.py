86490b90c6c4b25db658246be125a8d8e25f6b78
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-08-20 01:09:33 -0300

    Complete transfered the page to snippets

--- code137_website/controllers/main.py
+++ code137_website/controllers/main.py
@@ -3,17 +3,6 @@ from odoo.http import request
 from odoo.addons.website.controllers.main import Website
 
 
-class Website(Website):
-    @http.route("/", type="http", auth="public", website=True)
-    def index(self, **kw):
-        super(Website, self).index(**kw)
-        vals = {}
-        if kw.get("message_sent"):
-            message = "Sua mensagem foi enviada com sucesso!"
-            vals.update({"message": message})
-        return request.render("code137_website.homepage", vals)
-
-
 class Code137Controller(http.Controller):
     @http.route("/form-create-lead", type="http", auth="public", website=True)
     def form_create_lead(self, access_token=False, **kw):
