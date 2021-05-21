c185635b2cd1421a839eef9b1fa00cfb9f5c922c
Marcelo Costa <marceloengecom@gmail.com>
2021-05-20 21:01:51 -0300

    ..

--- payment_paghiper/models/paghiper.py
+++ payment_paghiper/models/paghiper.py
@@ -108,6 +108,7 @@ class PagHiperBoleto(models.Model):
 
 class TransactionPagHiper(models.Model):
     _inherit = "payment.transaction"
+    _inherit = "account.move"
 
     boleto_url = fields.Char(string="Fatura", size=300)
     boleto_digitable_line = fields.Char(string="Linha Digitável")
