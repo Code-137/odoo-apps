f710a739a98fad415aa4d11f6de12b8ff946ecd1
Felipe Paloschi <paloschi.eca@gmail.com>
2021-08-09 18:14:51 -0300

    Update code137_website templates

--- code137_website/controllers/main.py
+++ code137_website/controllers/main.py
@@ -3,13 +3,18 @@ from odoo.http import request
 
 
 class Code137Controller(http.Controller):
-    @http.route("/form-create-lead", type="http", auth="public", website=True)
+    @http.route(
+        "/form-create-lead",
+        type="http",
+        auth="public",
+        methods=["POST"],
+        website=True,
+    )
     def form_create_lead(self, access_token=False, **kw):
         vals = {
             "name": kw.get("name"),
-            "contact_name": kw.get("name"),
-            "email_from": kw.get("mail"),
-            "description": kw.get("description"),
+            "email_from": kw.get("email_from"),
+            "phone": kw.get("phone"),
         }
         partner = request.env["res.partner"].search(
             [("email", "=", kw.get("mail"))], limit=1
@@ -17,4 +22,14 @@ class Code137Controller(http.Controller):
         if partner:
             vals.update({"partner_id": partner.id})
         request.env["crm.lead"].sudo().create(vals)
-        return request.redirect("/?message_sent=1#code137-contact")
+        return request.redirect("/form-success")
+
+    @http.route(
+        "/form-success",
+        type="http",
+        auth="public",
+        website=True,
+        methods=["GET"],
+    )
+    def form_success(self):
+        return request.render("code137_website.form_success_page", {})
