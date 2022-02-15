6851d5c342a95912ba8eee1739e1b4e36b08e8f2
Felipe Paloschi <paloschi.eca@gmail.com>
2022-02-15 14:23:02 -0300

    Fix errors on delivery_correios

--- delivery_correios/__manifest__.py
+++ delivery_correios/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": """Integração com os Correios \
     Módulo para gerar etiquetas de rastreamento de \
     encomendas""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "MRP",
     "author": "Code 137",
     "license": "Other OSI approved licence",
