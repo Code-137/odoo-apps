696469658499c397e255e8d5509cb2282a76af24
Felipe Paloschi <paloschi.eca@gmail.com>
2022-08-25 15:25:43 -0300

    Add module code137_hide_action

---
+++ code137_hide_action/__manifest__.py
@@ -0,0 +1,18 @@
+# pylint: skip-file
+{
+    "name": "Hide Action Button",
+    "summary": """Hide 'Action' button from tree and form views""",
+    "category": "Extra Tools",
+    "license": "Other OSI approved licence",
+    "version": "14.0.1.0.0",
+    "author": "Code 137",
+    "website": "https://www.code137.com.br",
+    "contributors": [
+        "Felipe Paloschi <paloschi.eca@gmail.com>",
+    ],
+    "depends": ["web"],
+    "data": [
+        "security/groups.xml",
+        "views/assets.xml"
+    ],
+}
